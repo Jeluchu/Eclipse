c791b9b17e49d2808fe24b1aaf242afeecbf98f9
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-01 09:20:13 +0100

    INITIAL COMMIT

---
+++ 2017/Clase/Proyecto Prueba/src/Proyecto_Prueba.java
@@ -0,0 +1,23 @@
+
+public class Proyecto_Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int numero = 3;
+		double numero2 = 1.6; 
+		float numero3 = 2.5f;
+		char caracter = 'j';
+		boolean madrileño = true;
+		String cadena = "pepito01";
+		
+		System.out.print("Mi sobrina tiene "+numero+" años..."+"\t");
+		System.out.print(cadena);*/
+	
+		int lado = 5;
+		int perimetro = 4*lado;
+		int area = lado*lado;
+		
+		System.out.println("El perímetro del cuadrado es: "+perimetro);
+		System.out.println("El area del cuadrado es: "+area);
+	}
+}
