c85f232c8fff1ca3a178d3bb75ec86ffb30ee514
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-24 18:24:46 +0100

    CONDICIONALES 3

---
+++ 2017/Clase/Condicionales3/src/ifelse.java
@@ -0,0 +1,16 @@
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] conjunto = {3, 7, 5, 2, 9};
+		
+		for (int i=0; i<=4; i++) {
+			if(conjunto[i]%2==0) {
+				System.out.println(conjunto[i]);
+			}
+		}
+	}
+
+}
