79ac8e0beb956201653b8a12dcb71ac79dd3fb83
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-24 18:23:40 +0100

    CONDICIONALES 2

---
+++ 2017/Clase/Condicionales 2/src/ifelse.java
@@ -0,0 +1,18 @@
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 4;
+		int b = 8;
+			
+		if(a==4 || b==788) {
+			System.out.println("Correcto");
+		}
+		else {
+			System.out.println("Incorrecto");
+		}
+	}
+
+}
