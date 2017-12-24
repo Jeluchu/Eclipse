fa24e1e292dd704bb3f5781908489865212b4abb
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-24 18:23:09 +0100

    CONDICIONALES 1

---
+++ 2017/Clase/Condicionales/src/ifelse.java
@@ -0,0 +1,17 @@
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "Txema";
+			
+		if(a.equals("Txema")) {
+			System.out.println("Correcto");
+		}
+		else {
+			System.out.println("Incorrecto");
+		}
+	}
+
+}
