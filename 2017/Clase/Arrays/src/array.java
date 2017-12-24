b8f87c68a63dea70f902c3f6a1acbd7d6d71f7d3
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-24 18:21:25 +0100

    ARRAYS JAVA

---
+++ 2017/Clase/Arrays/src/array.java
@@ -0,0 +1,15 @@
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char [] conjunto = {'m', 'u', 'r', 'c', 'i', 'e', 'l', 'a', 'g', 'o'};
+						   
+		for(int i=0; i<=conjunto.length-1; i++) {
+		
+		System.out.println(conjunto[i]);
+		}
+ 	}
+
+}
