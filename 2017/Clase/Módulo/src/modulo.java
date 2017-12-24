45fe2552c04c7734a553fbb246f58e11579ddd87
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-24 18:26:30 +0100

    MÓDULO

---
+++ 2017/Clase/Módulo/src/modulo.java
@@ -0,0 +1,15 @@
+
+public class modulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] conjunto = {3, 4, 6, 9, 12};
+		//		  i		   0  1  2  3  4 
+		   
+		for(int i=0; i<=conjunto.length-1; i++) {
+		System.out.println(conjunto[i]);
+	}
+	}
+
+}
