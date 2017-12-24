43801750bd1958c8ca28206602de53a7c5e2b9d5
Jeluchu <jesusmarcalderon@gmail.com>
2017-12-24 18:25:27 +0100

    CONDICIONALES 4

---
+++ 2017/Clase/Condicional4/src/ifelse.java
@@ -0,0 +1,16 @@
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] personas = {"Ana", "Pepe", "Juan", "Luz"};
+	
+		for (int i=0; i<=3; i++) {
+			if(personas[i].equals("Pepe")) {
+				System.out.println(1);
+			}
+		}
+	}
+
+}
